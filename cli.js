ef8cfe97bce737cd564d4d9f12ee6c03ddd20a0c
Beau Raines <beau.raines@gmail.com>
2024-05-26 13:10:48 -0700

    feat: initial commit cli
    Only suppports the type and number of paragraphs options

---
+++ cli.js
@@ -0,0 +1,64 @@
+const fetch = require('node-fetch');
+const yargs = require('yargs');
+const copyPaste = require('copy-paste');
+const { alias } = require('yargs');
+const { describe } = require('yargs');
+
+const apiUrl = 'https://baconipsum.com/api/';
+
+// Define command-line options using yargs
+const argv = yargs
+  .scriptName('bacon')
+  .option('type', {
+    alias: 't',
+    describe: 'Type of meat',
+    default: 'all-meat',
+    choices: ['all-meat', 'meat-and-filler'],
+  })
+  .option('paras', {
+    alias: 'p',
+    describe: 'Number of paragraphs',
+    default: 1,
+    type: 'number',
+  })
+  .option('sentences',{
+    alias: 's',
+    describe: 'Number of sentences. This overrides paragraphs.',
+    type: 'number'
+  })
+  .option('start-with-lorem',{
+    alias: ['l','lorem'],
+    describe: 'Starts the first paragraph with ‘Bacon ipsum dolor sit amet’',
+    type: 'boolean'
+  })
+  .option('format',{
+    alias: 'f',
+    describe:'Output format. Defaults to `text`',
+    default: 'text',
+    choices: ['text','json','html']
+  })
+  .option('no-clip',{
+    alias: 'nc',
+    describe:'Only outputs to console, bypassing the clipboard.',
+    type: 'boolean'
+  })
+  .help()
+  .argv;
+  
+// Build the API URL with the specified options
+// TODO support additional parameters
+const apiUrlWithParams = `${apiUrl}?type=${argv.type}&paras=${argv.paras}`;
+
+// Fetch data from the Bacon Ipsum API
+fetch(apiUrlWithParams)
+  .then((response) => response.json())
+  .then((data) => {
+    // Display the fetched text in the console
+    console.log(data.join('\n'));
+
+    // Copy the text to the clipboard
+    copyPaste.copy(data.join('\n'), () => {
+      console.log('Text copied to clipboard!');
+    });
+  })
+  .catch((error) => console.error('Error fetching data:', error));
