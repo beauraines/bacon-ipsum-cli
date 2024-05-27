7c34b5d24f6d4bcb4445e4a6fc9271a55a7c5d4d
Beau Raines <beau.raines@gmail.com>
2024-05-27 08:01:42 -0700

    fix: bad merge in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,15 +5,9 @@ import jestPlugin from "eslint-plugin-jest";
 export default [
   {
     files: ["**/*.js"],
-<<<<<<< HEAD
     languageOptions: { sourceType: "commonjs", globals: globals.nodeBuiltin}
   },
   { languageOptions: { ...jestPlugin.environments.globals } },
-=======
-    languageOptions: { sourceType: "commonjs" }
-  },
-  { languageOptions: { globals: globals.browser,  ...jestPlugin.environments.globals } },
->>>>>>> ec732ac2838a23ef13bb006d6bf566923f669249
   pluginJs.configs.recommended,
   {
     plugins: {
